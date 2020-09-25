9074a0e6d292f07c6ddd06ffae8f827688d94b74
Rakib Ansary <rakibansary@gmail.com>
2020-09-25 19:24:17 +0600

    update

--- conf.py
+++ conf.py
@@ -44,12 +44,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-source_parsers = {
-    '.md': 'recommonmark.parser.CommonMarkParser',
-}
-
-source_suffix = ['.rst', '.md']
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
